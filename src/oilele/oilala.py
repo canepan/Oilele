2c4b6c37ac4191937c3fd704e57793388ec389b0
Nicola Canepa <canne74@gmail.com>
2022-10-19 08:18:41 +0100

    Add support for CBZ

--- src/oilele/oilala.py
+++ src/oilele/oilala.py
@@ -20,11 +20,12 @@ def parse_args(argv: list):
 class ComicDisplay(object):
     screen: pygame.Surface = attr.ib()
     images: list = attr.ib()
+    log = attr.ib()
     curr_index: int = attr.ib(default=0)
     visible: bool = attr.ib(default=False)
     rotate: int = attr.ib(converter=int, default=0)
     ratio: float = attr.ib(default=1.0)
-    file_name: str = attr.ib(default='')
+    file_name: str = attr.ib(default='', converter=os.path.basename)
 
     @property
     def curr_image(self):
@@ -45,15 +46,15 @@ class ComicDisplay(object):
     def show(self, image_changed=True):
         self.visible = True
         if image_changed:
-            image = self.curr_image
-            surf = pygame.image.fromstring(image.tobytes(), image.size, image.mode).convert()
-            self.ratio = image.height / image.width
+            surf = self.curr_image
+            self.ratio = surf.get_height() / surf.get_width()
             if self.rotate:
                 surf = pygame.transform.rotate(surf, self.rotate)
             pygame.display.set_caption(f'{self.curr_index}/{len(self.images)} - {self.file_name}')
             self.surf = surf
         else:
             surf = self.surf
+        self.log.debug(f'Surface: {surf}')
         new_size = self.screen.get_size()
         new_size = (self.ratio * new_size[1], new_size[1])
         surf = pygame.transform.smoothscale(surf, new_size)
@@ -61,23 +62,45 @@ class ComicDisplay(object):
         pygame.display.flip()
 
 
+@attr.s
+class ComicDisplayPil(ComicDisplay):
+    @property
+    def curr_image(self):
+        image = self.images[self.curr_index]
+        self.log.debug(f'Image: {image}')
+        return pygame.image.fromstring(image.tobytes(), image.size, image.mode).convert('RGB')
+
+
+def is_mouse_or_key(mouse_button, key) -> bool:
+    return (
+        (pyg_event.type == pygame.MOUSEBUTTONDOWN and pyg_event.button == mouse_button)
+        or (pyg_event.type == pygame.KEYDOWN and pyg_event.key == key)
+    )
+
+
 def is_next_event(pyg_event) -> bool:
-    return getattr(pyg_event, 'key', getattr(pyg_event, 'button', None)) in (pygame.K_RIGHT, 1)
+    return is_mouse_or_key(1, pygame.K_RIGHT)
 
 
 def is_prev_event(pyg_event) -> bool:
-    return getattr(pyg_event, 'key', getattr(pyg_event, 'button', None)) in (pygame.K_LEFT, 3)
+    return is_mouse_or_key(3, pygame.K_LEFT)
 
 
 def is_quit_event(pyg_event) -> bool:
     return pyg_event.type == pygame.QUIT or getattr(pyg_event, 'key', None) == pygame.K_q
 
 
-def images_from_archive(file_name: str) -> list:
+def images_from_archive(file_name: str, log) -> list:
     images_list = list()
     with ZipFile(file_name, 'r') as archive_file:
         for image_name in sorted(archive_file.namelist()):
-            images_list.append(Image.open(archive_file.open(image_name, 'r')))
+            try:
+                log.debug(image_name)
+                images_list.append(pygame.image.load(archive_file.open(image_name, 'r')))
+            # except PIL.UnidentifiedImageError as e:
+            except Exception as e:
+                log.exception(e)
+                log.error(f'Error while adding {image_name} as an image from {file_name}: {e}')
     return images_list
 
 
@@ -88,37 +111,38 @@ def main(argv=None):
 
     pygame.display.init()
     # print(pygame.display.get_desktop_sizes())
-    # screen = pygame.display.set_mode((800, 600), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
-    screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+    screen = pygame.display.set_mode((800, 600), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
+    # screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
     pygame.display.set_caption(f'Loading {cfg.filein}...')
     try:
         pdf_info = pdfinfo_from_path(cfg.filein)
         cfg.log.info(f'pdf_info: {pdf_info}')
-        images = ComicDisplay(screen, convert_from_path(cfg.filein), rotate=pdf_info.get('Page rot'), file_name=cfg.filein)
-    except Exception:
-        images = ComicDisplay(screen, images_from_archive(cfg.filein), file_name=cfg.filein)
+        images = ComicDisplayPil(
+            screen, convert_from_path(cfg.filein), cfg.log, rotate=pdf_info.get('Page rot'), file_name=cfg.filein
+        )
+    except pdf2image.exceptions.PDFPageCountError as e:
+        cfg.log.debug(e)
+        images = ComicDisplay(screen, images_from_archive(cfg.filein, cfg.log), cfg.log, file_name=cfg.filein)
 
     images.show()
     looping = True
     while looping:
         for pyg_event in pygame.event.get():
             if is_quit_event(pyg_event):
-                print(pyg_event)
+                cfg.log.debug(pyg_event)
                 looping = False
-            elif pyg_event.type == pygame.MOUSEBUTTONDOWN or pyg_event.type == pygame.KEYDOWN:
-                # print(pyg_event)
-                if is_next_event(pyg_event):
-                    images.next()
-                elif is_prev_event(pyg_event):
-                    images.prev()
+            elif is_next_event(pyg_event):
+                images.next()
+            elif is_prev_event(pyg_event):
+                images.prev()
             elif pyg_event.type == pygame.VIDEORESIZE:
-                print('VIDEORESIZE')
+                cfg.log.debug('VIDEORESIZE')
                 # images.show(image_changed=False)
             elif pyg_event.type == pygame.WINDOWSIZECHANGED:
-                print('WINDOWSIZECHANGED')
+                cfg.log.debug('WINDOWSIZECHANGED')
                 images.show(image_changed=False)
             elif pyg_event.type != pygame.MOUSEMOTION:
-                print(pyg_event)
+                cfg.log.debug(pyg_event)
 
     pygame.quit()
 
